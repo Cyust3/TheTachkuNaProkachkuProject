4d55256775b90bef77c7da678a1bd3100846dcf6
Cyust <a_berdyaev@mail.ru>
2025-05-13 21:12:32 +0400

    Исключение

---
+++ TheTachkuNaProkachkuConctrat/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace TheTachkuNaProkachkuConctrat.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static bool IsDateNotOlder(this DateTime date, DateTime olderDate)
+    {
+        return date >= olderDate;
+    }
+}
